95d059ebab6f2aae904b9d3df97769b0ed98a9f7
Michael Kleyman <michaelkleyman99@gmail.com>
2023-03-22 20:57:38 -0400

    Revert "ng back to previous commit"
    This reverts commit 357b78868244ad26034b57380f2ec7a287631f6b.
    
    message

---
+++ pt-me/src/server/db/database.tsx
@@ -0,0 +1,10 @@
+const Sequelize = require('sequelize');
+
+const databaseName = 'pt-me';
+
+const db = new Sequelize(`postgres://localhost:5432/${databaseName}`, {
+  logging: false, //hides the sequelize queries I believe
+});
+
+// module.exports = db;
+export default db
---
+++ pt-me/src/server/index.tsx
@@ -0,0 +1,6 @@
+export {}
+
+const express = require('express');
+const app = express()
+
+export default app
