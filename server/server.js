c5d76f46a55d17222b7ff32441f6f23afa682530
Michael Kleyman <michaelkleyman99@gmail.com>
2023-04-15 15:07:54 -0400

    installed middlewares for server logs, allowing cross origin resource sharing, and body parsing for request objects

--- server/server.js
+++ server/server.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
 const app = express();
 
 const PORT = 3001;
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.send('Server is running!');
