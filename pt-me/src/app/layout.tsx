bd2bf8f4847495acff06c084c99777daa25009af
Michael Kleyman <michaelkleyman99@gmail.com>
2023-04-17 14:22:18 -0400

    setting up redux toolkit files for later use

--- pt-me/src/app/layout.tsx
+++ pt-me/src/app/layout.tsx
@@ -2,7 +2,6 @@
 import '../styles/globals.css';
 import Navbar from '../components/Navbar';
 import { ThemeProvider } from 'next-themes';
-import { Provider } from 'react-redux';
 
 export default function RootLayout({
   children,
@@ -13,10 +12,8 @@ export default function RootLayout({
     <html lang='en'>
       <body>
         <ThemeProvider enableSystem={true} attribute='class'>
-          {/* <Provider> */}
           <Navbar />
           {children}
-          {/* </Provider> */}
         </ThemeProvider>
       </body>
     </html>
