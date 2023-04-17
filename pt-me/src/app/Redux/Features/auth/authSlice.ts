f9390d558945c9f57afa949bcf5026ec2135b736
Michael Kleyman <michaelkleyman99@gmail.com>
2023-04-17 16:23:02 -0400

    used redux thunk function to make a call in the react component properly with typescript

--- pt-me/src/app/Redux/Features/auth/authSlice.ts
+++ pt-me/src/app/Redux/Features/auth/authSlice.ts
@@ -16,10 +16,15 @@ const initialState: AuthState = {
   error: null,
 };
 
-interface AuthenticatePayload {
-  username: string;
+interface SignupPayload {
+  clinicName: string;
+  address: string;
+  email: string;
   password: string;
-  method: string;
+}
+interface LoginPayload {
+  email: string;
+  password: string;
 }
 
 export const me = createAsyncThunk(
@@ -27,7 +32,7 @@ export const me = createAsyncThunk(
   async (_, { dispatch }) => {
     const token = window.localStorage.getItem(TOKEN);
     if (token) {
-      const res = await Axios.get('/auth/me', {
+      const res = await Axios.get(`${BASE_URL}/auth/me`, {
         headers: {
           authorization: token,
         },
@@ -37,17 +42,38 @@ export const me = createAsyncThunk(
   }
 );
 
-export const authenticate = createAsyncThunk(
-  'auth/authenticate',
-  async ({ username, password, method }: AuthenticatePayload, { dispatch }) => {
+export const signup = createAsyncThunk(
+  'auth/signup',
+  async (
+    { clinicName, address, email, password }: SignupPayload,
+    { dispatch }
+  ) => {
     try {
-      const res = await Axios.post(`${BASE_URL}/auth/${method}`, {
-        username,
+      const res = await Axios.post(`${BASE_URL}/auth/signup`, {
+        clinicName,
+        address,
+        email,
         password,
       });
       window.localStorage.setItem(TOKEN, res.data.token);
       await dispatch(me());
-    } catch (authError) {
+    } catch (authError: any) {
+      return { error: authError.message };
+    }
+  }
+);
+
+export const login = createAsyncThunk(
+  'auth/login',
+  async ({ email, password }: LoginPayload, { dispatch }) => {
+    try {
+      const res = await Axios.post(`${BASE_URL}/auth/login`, {
+        email,
+        password,
+      });
+      window.localStorage.setItem(TOKEN, res.data.token);
+      await dispatch(me());
+    } catch (authError: any) {
       return { error: authError.message };
     }
   }
@@ -66,7 +92,11 @@ export const authSlice = createSlice({
       .addCase(me.fulfilled, (state, action) => {
         state.user = action.payload;
       })
-      .addCase(authenticate.fulfilled, (state, action: PayloadAction<any>) => {
+      .addCase(signup.fulfilled, (state, action: PayloadAction<any>) => {
+        state.user = action.payload;
+        state.error = action.payload?.error ?? null;
+      })
+      .addCase(login.fulfilled, (state, action: PayloadAction<any>) => {
         state.user = action.payload;
         state.error = action.payload?.error ?? null;
       });
