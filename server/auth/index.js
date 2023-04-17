3c732c937c1bc1833245946f3a9cb09edae36256
Michael Kleyman <michaelkleyman99@gmail.com>
2023-04-17 09:47:15 -0400

    setup api routes for authorization to allow for user login and signup via json web token

---
+++ server/auth/index.js
@@ -0,0 +1,36 @@
+const router = require('express').Router();
+const User = require('../models/user');
+
+//GET /auth/me
+router.get('/me', async (req, res, next) => {
+  try {
+    res.send(await User.findByToken(req.headers.authorization));
+  } catch (error) {
+    next(error);
+  }
+});
+
+//POST /auth/login
+router.post('/login', async (req, res, next) => {
+  try {
+    res.send({ token: await User.authenticate(req.body) });
+  } catch (error) {
+    next(error);
+  }
+});
+
+//POST /auth/signup
+router.post('/signup', async (req, res, next) => {
+  try {
+    const newUser = await User.create(req.body);
+    res.send({ token: await newUser.generateToken() });
+  } catch (error) {
+    if (error.name === 'SequelizeUniqueConstraintError') {
+      res.status(401).send('User Already Exists');
+    } else {
+      next(error);
+    }
+  }
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -14,6 +14,9 @@ app.get('/', (req, res) => {
   res.send('Server is running!');
 });
 
+app.use('/auth', require('./auth'));
+app.use('/api', require('./routes'));
+
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
