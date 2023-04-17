f683148e1514eb07aa6264ce5f6af2ef5e7a6558
Michael Kleyman <michaelkleyman99@gmail.com>
2023-04-17 15:51:09 -0400

    completed the auth reducer

--- pt-me/src/app/Redux/Features/auth/authSlice.ts
+++ pt-me/src/app/Redux/Features/auth/authSlice.ts
@@ -1,35 +1,76 @@
 'use client';
 
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
 import Axios from 'axios';
 
 const TOKEN = 'token';
 const BASE_URL = 'http://localhost:3001';
 
-export interface AuthState {
-  //   user: Object | null;
-  //   token: Text | null;
+interface AuthState {
+  user: any; // Replace 'any' with your user type
+  error: string | null;
 }
 
 const initialState: AuthState = {
-  //   user: null,
-  //   token: null,
+  user: null,
+  error: null,
+};
+
+interface AuthenticatePayload {
+  username: string;
+  password: string;
+  method: string;
+}
+
+export const me = createAsyncThunk(
+  `${BASE_URL}/auth/me`,
+  async (_, { dispatch }) => {
+    const token = window.localStorage.getItem(TOKEN);
+    if (token) {
+      const res = await Axios.get('/auth/me', {
+        headers: {
+          authorization: token,
+        },
+      });
+      return res.data;
+    }
+  }
+);
+
+export const authenticate = createAsyncThunk(
+  'auth/authenticate',
+  async ({ username, password, method }: AuthenticatePayload, { dispatch }) => {
+    try {
+      const res = await Axios.post(`${BASE_URL}/auth/${method}`, {
+        username,
+        password,
+      });
+      window.localStorage.setItem(TOKEN, res.data.token);
+      await dispatch(me());
+    } catch (authError) {
+      return { error: authError.message };
+    }
+  }
+);
+
+export const logout = () => {
+  window.localStorage.removeItem(TOKEN);
 };
 
 export const authSlice = createSlice({
   name: 'auth',
   initialState,
-  reducers: {
-    me: async () => {
-      const token = window.localStorage.getItem(TOKEN);
-      if (token) {
-        const res = await Axios.get(`${BASE_URL}/auth/me`, {
-          headers: {
-            authorization: token,
-          },
-        });
-        return res.data;
-      }
-    },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(me.fulfilled, (state, action) => {
+        state.user = action.payload;
+      })
+      .addCase(authenticate.fulfilled, (state, action: PayloadAction<any>) => {
+        state.user = action.payload;
+        state.error = action.payload?.error ?? null;
+      });
   },
 });
+
+export default authSlice.reducer;
