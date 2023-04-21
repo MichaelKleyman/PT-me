bb63d7fe5683173e81a4076489691aef0477a236
Michael Kleyman <michaelkleyman99@gmail.com>
2023-04-21 16:45:07 -0400

    changed database name in config file

--- server/config/config.js
+++ server/config/config.js
@@ -3,19 +3,19 @@ module.exports = {
   development: {
     username: 'michaelkleyman',
     // password: null,
-    database: 'pt-me',
+    database: 'pt_me',
     dialect: 'postgres',
   },
   test: {
     username: 'michaelkleyman',
     // password: null,
-    database: 'pt-me',
+    database: 'pt_me',
     dialect: 'postgres',
   },
   production: {
     username: 'michaelkleyman',
     // password: null,
-    database: 'pt-me',
+    database: 'pt_me',
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
     dialectOptions: {
