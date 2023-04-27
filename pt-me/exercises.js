87efd03d6bdf48e13368ac2ccebcbae2a2ba1b03
Michael Kleyman <michaelkleyman99@gmail.com>
2023-04-26 20:39:44 -0400

    made temp exercise file on the frontend to setup proper UI for exercise page

---
+++ pt-me/exercises.js
@@ -0,0 +1,59 @@
+export const shoulderExercises = [
+  { name: 'External rotation' },
+  { name: 'External rotation' },
+  { name: 'External rotation' },
+  { name: 'External rotation' },
+  { name: 'External rotation' },
+  { name: 'External rotation' },
+  { name: 'External rotation' },
+  { name: 'External rotation' },
+  { name: 'External rotation' },
+  { name: 'External rotation' },
+  { name: 'External rotation' },
+  { name: 'External rotation' },
+];
+
+export const backExercises = [
+  { name: 'Childs Pose' },
+  { name: 'Childs Pose' },
+  { name: 'Childs Pose' },
+  { name: 'Childs Pose' },
+  { name: 'Childs Pose' },
+  { name: 'Childs Pose' },
+  { name: 'Childs Pose' },
+  { name: 'Childs Pose' },
+  { name: 'Childs Pose' },
+  { name: 'Childs Pose' },
+  { name: 'Childs Pose' },
+  { name: 'Childs Pose' },
+];
+
+export const kneeExercises = [
+  { name: 'Quad Stretch' },
+  { name: 'Quad Stretch' },
+  { name: 'Quad Stretch' },
+  { name: 'Quad Stretch' },
+  { name: 'Quad Stretch' },
+  { name: 'Quad Stretch' },
+  { name: 'Quad Stretch' },
+  { name: 'Quad Stretch' },
+  { name: 'Quad Stretch' },
+  { name: 'Quad Stretch' },
+  { name: 'Quad Stretch' },
+  { name: 'Quad Stretch' },
+];
+
+export const hipExercises = [
+  { name: 'External hip rotation' },
+  { name: 'External hip rotation' },
+  { name: 'External hip rotation' },
+  { name: 'External hip rotation' },
+  { name: 'External hip rotation' },
+  { name: 'External hip rotation' },
+  { name: 'External hip rotation' },
+  { name: 'External hip rotation' },
+  { name: 'External hip rotation' },
+  { name: 'External hip rotation' },
+  { name: 'External hip rotation' },
+  { name: 'External hip rotation' },
+];
--- pt-me/src/app/loading.tsx
+++ pt-me/src/app/loading.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+'use client';
+import CircularProgress from '@mui/material/CircularProgress';
 
 export default function loading() {
   return (
     <div className='flex items-center justify-center mt-[20rem]'>
-      loading...
+      <CircularProgress />
     </div>
   );
 }
