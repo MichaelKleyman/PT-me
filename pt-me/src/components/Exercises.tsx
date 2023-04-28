5b2b25d6be003badb3d78440b68d3fc94fe34aca
Michael Kleyman <michaelkleyman99@gmail.com>
2023-04-28 16:52:57 -0400

    built menu for filtering exercises by injury/body part

--- pt-me/src/components/Exercises.tsx
+++ pt-me/src/components/Exercises.tsx
@@ -4,6 +4,7 @@ import { CLIENT, BASE_URL } from '@/components/api';
 
 export default function Exercises() {
   const [exercises, setExercises] = useState<any>({});
+  const [selected, setSelected] = useState<String>('All');
 
   useEffect(() => {
     async function getExercises() {
@@ -15,5 +16,32 @@ export default function Exercises() {
 
   console.log(exercises);
 
-  return <div>Exercises</div>;
+  return (
+    <div>
+      <h1 className='text-green-500 text-xl uppercase tracking-widest'>
+        Exercises
+      </h1>
+      <div className='mt-1 flex'>
+        <div
+          className={`${
+            selected === 'All' ? 'bg-green-500 text-white' : ''
+          } p-2 cursor-pointer border border-green-500 m-2 rounded-lg hover:bg-green-500 hover:text-white`}
+        >
+          All
+        </div>
+        {Object.keys(exercises).map((exerciseType, i) => (
+          <button
+            onClick={() => setSelected(exerciseType)}
+            key={i}
+            className={`${
+              selected === exerciseType ? 'bg-green-500 text-white' : ''
+            } p-2 cursor-pointer border border-green-500 m-2 rounded-lg hover:bg-green-500 hover:text-white`}
+          >
+            {exerciseType}
+          </button>
+        ))}
+      </div>
+      <div>{selected}</div>
+    </div>
+  );
 }
