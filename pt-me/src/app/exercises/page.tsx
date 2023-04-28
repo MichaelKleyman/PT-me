888c8cb77873b817dfa7e3b64d5d015ea45e892b
Michael Kleyman <michaelkleyman99@gmail.com>
2023-04-28 09:41:23 -0400

    temp frontend route to fetch all exercises is successful

--- pt-me/src/app/exercises/page.tsx
+++ pt-me/src/app/exercises/page.tsx
@@ -1,6 +1,7 @@
 import Exercises from '@/components/Exercises';
 
 export default function page() {
+
   return (
     <div className='mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl mt-[10rem]'>
       <Exercises />
--- pt-me/src/components/Exercises.tsx
+++ pt-me/src/components/Exercises.tsx
@@ -1,5 +1,19 @@
-import React from 'react';
+'use client';
+import React, { useEffect, useState } from 'react';
+import { CLIENT, BASE_URL } from '@/components/api';
 
 export default function Exercises() {
+  const [exercises, setExercises] = useState<any>({});
+
+  useEffect(() => {
+    async function getExercises() {
+      const { data } = await CLIENT.get(`${BASE_URL}/api/exercises`);
+      setExercises(data);
+    }
+    getExercises();
+  }, []);
+
+  console.log(exercises);
+
   return <div>Exercises</div>;
 }
