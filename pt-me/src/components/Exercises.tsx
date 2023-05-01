805691527545b4bdf01ccc8bd817e3660a306885
Michael Kleyman <michaelkleyman99@gmail.com>
2023-05-01 19:31:50 -0400

    added typescript fix to the state type in exericse display however it is temporary

--- pt-me/src/components/Exercises.tsx
+++ pt-me/src/components/Exercises.tsx
@@ -7,12 +7,17 @@ interface Exercise {
 }
 
 export default function Exercises() {
+  //exercises state is an object, where each key is a string and each value is an array of objects
   const [exercises, setExercises] = useState<Record<string, Exercise>>({});
-  const [specificExercise, setSpecificExercise] = useState<Array<Exercise>>([]);
+  //specific exercises state is an array of objects where each key is a string and each value is a a string.
+  const [specificExercise, setSpecificExercise] = useState<Array<Exercise[]>>(
+    []
+  );
   const [selected, setSelected] = useState<String>('All');
 
   const getAllExercises = async () => {
     const { data } = await CLIENT.get(`${BASE_URL}/api/exercises`);
+    // console.log(data);
     setExercises(data);
   };
 
@@ -26,16 +31,23 @@ export default function Exercises() {
       getAllExercises();
       setSpecificExercise([]);
     } else {
-      const filteredExerciseType = Object.keys(exercises).filter((exercise) => {
-        if (exercise === exerciseType) {
-          return exercise;
+      const filteredExerciseType = Object.keys(exercises).filter(
+        (exercise: String) => {
+          if (exercise === exerciseType) {
+            return exercise;
+          }
         }
-      });
-      setSpecificExercise(exercises[filteredExerciseType[0]]);
+      );
+      // console.log(exercises);
+      const arr = exercises[filteredExerciseType[0]];
+      setSpecificExercise([arr] as any);
     }
   };
-  console.log('Exercises: ', exercises);
+
   console.log('Specific Exercise: ', specificExercise);
+  // specificExercise.forEach((elem) => {
+  //   console.log(elem);
+  // });
 
   return (
     <div>
@@ -63,13 +75,15 @@ export default function Exercises() {
           </button>
         ))}
       </div>
-      <div>
+      <div className='border border-gray-200 mt-3 w-[100%]'>
         {specificExercise.length ? (
-          <>
-            {specificExercise.map((exercise: any, i: any) => (
-              <div key={i}>{exercise.name}</div>
+          <div className='grid grid-cols-3 gap-1 mt-3 m-5'>
+            {specificExercise[0].map((exercise: Exercise, i: any) => (
+              <div key={i} className='border border-gray-200 rounded-lg'>
+                <div className='p-3'>{exercise.name}</div>
+              </div>
             ))}
-          </>
+          </div>
         ) : (
           <>
             {Object.keys(exercises).map((exercise: any, i: any) => (
