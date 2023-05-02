cdcf1cc05ec511b619edc0d606ff3496c1443b56
Michael Kleyman <michaelkleyman99@gmail.com>
2023-05-02 19:16:09 -0400

    Ran migrationsand seed commands for the exercises dummy data to integrate into postgres

--- server/seeders/20230425005656-Exercises.js
+++ server/seeders/20230425005656-Exercises.js
@@ -12,15 +12,302 @@ module.exports = {
      *   isBetaMember: false
      * }], {});
      */
-    await queryInterface.bulkInsert(
-      'Exercises',
-      [
-        {
-          name: 'John Doe',
-        },
-      ],
-      {}
-    );
+    await queryInterface.bulkInsert('Exercises', [
+      {
+        name: 'External rotation',
+        injuryId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'External rotation',
+        injuryId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'External rotation',
+        injuryId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'External rotation',
+        injuryId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'External rotation',
+        injuryId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'External rotation',
+        injuryId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'External rotation',
+        injuryId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'External rotation',
+        injuryId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'External rotation',
+        injuryId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'External rotation',
+        injuryId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'External rotation',
+        injuryId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'External rotation',
+        injuryId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'Childs Pose',
+        injuryId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'Childs Pose',
+        injuryId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'Childs Pose',
+        injuryId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'Childs Pose',
+        injuryId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'Childs Pose',
+        injuryId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'Childs Pose',
+        injuryId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'Childs Pose',
+        injuryId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'Childs Pose',
+        injuryId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'Childs Pose',
+        injuryId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'Childs Pose',
+        injuryId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'Childs Pose',
+        injuryId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'Childs Pose',
+        injuryId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'Childs Pose',
+        injuryId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'Quad Stretch',
+        injuryId: 3,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'Quad Stretch',
+        injuryId: 3,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'Quad Stretch',
+        injuryId: 3,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'Quad Stretch',
+        injuryId: 3,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'Quad Stretch',
+        injuryId: 3,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'Quad Stretch',
+        injuryId: 3,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'Quad Stretch',
+        injuryId: 3,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'Quad Stretch',
+        injuryId: 3,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'Quad Stretch',
+        injuryId: 3,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'Quad Stretch',
+        injuryId: 3,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'Quad Stretch',
+        injuryId: 3,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'Quad Stretch',
+        injuryId: 3,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'External hip rotation',
+        injuryId: 4,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'External hip rotation',
+        injuryId: 4,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'External hip rotation',
+        injuryId: 4,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'External hip rotation',
+        injuryId: 4,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'External hip rotation',
+        injuryId: 4,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'External hip rotation',
+        injuryId: 4,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'External hip rotation',
+        injuryId: 4,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'External hip rotation',
+        injuryId: 4,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'External hip rotation',
+        injuryId: 4,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'External hip rotation',
+        injuryId: 4,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'External hip rotation',
+        injuryId: 4,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'External hip rotation',
+        injuryId: 4,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
   },
 
   async down(queryInterface, Sequelize) {
@@ -30,5 +317,6 @@ module.exports = {
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
+    await queryInterface.bulkDelete('Exercises', null, {});
   },
 };
