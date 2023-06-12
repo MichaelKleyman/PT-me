8c19426984bfe8ae3007c20c69cf1e137271ab7e
Michael Kleyman <michaelkleyman99@gmail.com>
2023-06-12 16:20:03 -0400

    setup foundation for the patient exercise flow sheet which will hold a drag and drop style patient exercise workflow

--- pt-me/src/app/patient/[id]/page.tsx
+++ pt-me/src/app/patient/[id]/page.tsx
@@ -8,7 +8,7 @@ import Stack from '@mui/material/Stack';
 import { GrCalendar } from 'react-icons/gr';
 import { BsDot } from 'react-icons/bs';
 import { FiPhone } from 'react-icons/fi';
-import { MdOutlineEdit } from 'react-icons/md';
+import { MdOutlineEdit, MdOutlineTipsAndUpdates } from 'react-icons/md';
 import {
   AiOutlineMail,
   AiOutlineFileSearch,
@@ -184,8 +184,46 @@ export default function Patient({ params }: Params) {
         <div className='bg-[#fdfff5] p-7 shadow-lg shadow-gray-200 rounded-md w-[30%] text-lg uppercase tracking-widest'>
           Exercise List
         </div>
-        <div className='bg-[#fdfff5] p-7 shadow-lg shadow-gray-200 rounded-md w-[70%] text-lg uppercase tracking-widest'>
-          Schedule
+        <div className='bg-[#fdfff5] p-7 shadow-lg shadow-gray-200 rounded-md w-[70%] text-lg uppercase tracking-widest overflow-y-scroll'>
+          <div className='flex items-center justify-between'>
+            <h1>Schedule</h1>
+            <button className='text-[15px] text-blue-500 flex items-center hover:bg-[#fdfff5] hover:shadow-lg hover:shadow-gray-300 rounded-lg p-2 duration-300 hover:scale-110 cursor-pointer'>
+              <MdOutlineTipsAndUpdates className='p-2' size={35} />
+              Update
+            </button>
+          </div>
+          <table className='table-auto mt-4'>
+            <thead>
+              <tr>
+                <th className='border px-4 py-2'></th>
+                {Array.from({ length: 6 }).map((_, colIndex) => (
+                  <th key={colIndex} className='border px-4 py-2'>
+                    Date {colIndex + 1}
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th className='border px-4 py-2'>Exercise</th>
+                {Array.from({ length: 6 }).map((_, colIndex) => (
+                  <td key={colIndex} className='border px-4 py-2'>
+                    5 x 30
+                  </td>
+                ))}
+              </tr>
+              {Array.from({ length: 6 - 1 }).map((_, rowIndex) => (
+                <tr key={rowIndex + 1}>
+                  <th className='border px-4 py-2'>Exercise</th>
+                  {Array.from({ length: 6 }).map((_, colIndex) => (
+                    <td key={colIndex} className='border px-4 py-2'>
+                      5 x 30
+                    </td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
