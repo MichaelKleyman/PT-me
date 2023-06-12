1da9c0d0d1dd75140fd677ca1796189d9c2f488a
Michael Kleyman <michaelkleyman99@gmail.com>
2023-06-12 16:25:17 -0400

    added expand and update buttons to the schedule component, and made the table cells a little bigger for when the exercise names get dragged in

--- pt-me/src/app/patient/[id]/page.tsx
+++ pt-me/src/app/patient/[id]/page.tsx
@@ -8,6 +8,7 @@ import Stack from '@mui/material/Stack';
 import { GrCalendar } from 'react-icons/gr';
 import { BsDot } from 'react-icons/bs';
 import { FiPhone } from 'react-icons/fi';
+import { FaExpand } from 'react-icons/fa';
 import { MdOutlineEdit, MdOutlineTipsAndUpdates } from 'react-icons/md';
 import {
   AiOutlineMail,
@@ -187,17 +188,23 @@ export default function Patient({ params }: Params) {
         <div className='bg-[#fdfff5] p-7 shadow-lg shadow-gray-200 rounded-md w-[70%] text-lg uppercase tracking-widest overflow-y-scroll'>
           <div className='flex items-center justify-between'>
             <h1>Schedule</h1>
-            <button className='text-[15px] text-blue-500 flex items-center hover:bg-[#fdfff5] hover:shadow-lg hover:shadow-gray-300 rounded-lg p-2 duration-300 hover:scale-110 cursor-pointer'>
-              <MdOutlineTipsAndUpdates className='p-2' size={35} />
-              Update
-            </button>
+            <div className='flex justify-between items-center gap-2'>
+              <button className='text-[15px] text-blue-500 flex items-center hover:bg-[#fdfff5] hover:shadow-lg hover:shadow-gray-300 rounded-lg p-2 duration-300 hover:scale-110 cursor-pointer'>
+                <FaExpand className='p-2' size={35} />
+                Expand
+              </button>
+              <button className='text-[15px] text-blue-500 flex items-center hover:bg-[#fdfff5] hover:shadow-lg hover:shadow-gray-300 rounded-lg p-2 duration-300 hover:scale-110 cursor-pointer'>
+                <MdOutlineTipsAndUpdates className='p-2' size={35} />
+                Update
+              </button>
+            </div>
           </div>
           <table className='table-auto mt-4'>
             <thead>
               <tr>
-                <th className='border px-4 py-2'></th>
+                <th className='border px-6 py-4'></th>
                 {Array.from({ length: 6 }).map((_, colIndex) => (
-                  <th key={colIndex} className='border px-4 py-2'>
+                  <th key={colIndex} className='border px-6 py-4'>
                     Date {colIndex + 1}
                   </th>
                 ))}
@@ -205,18 +212,18 @@ export default function Patient({ params }: Params) {
             </thead>
             <tbody>
               <tr>
-                <th className='border px-4 py-2'>Exercise</th>
+                <th className='border px-6 py-4'>Exercise</th>
                 {Array.from({ length: 6 }).map((_, colIndex) => (
-                  <td key={colIndex} className='border px-4 py-2'>
+                  <td key={colIndex} className='border px-6 py-4'>
                     5 x 30
                   </td>
                 ))}
               </tr>
               {Array.from({ length: 6 - 1 }).map((_, rowIndex) => (
                 <tr key={rowIndex + 1}>
-                  <th className='border px-4 py-2'>Exercise</th>
+                  <th className='border px-6 py-4'>Exercise</th>
                   {Array.from({ length: 6 }).map((_, colIndex) => (
-                    <td key={colIndex} className='border px-4 py-2'>
+                    <td key={colIndex} className='border px-6 py-4'>
                       5 x 30
                     </td>
                   ))}
