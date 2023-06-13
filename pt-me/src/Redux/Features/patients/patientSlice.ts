75175b92638b7cebf75e3cde4316eae6de19acea
Michael Kleyman <michaelkleyman99@gmail.com>
2023-06-13 06:22:04 -0400

    plugged in new thunk into patient slice which communicates with the backend experess call to fetch get route for specific patient

--- pt-me/src/Redux/Features/patients/patientSlice.ts
+++ pt-me/src/Redux/Features/patients/patientSlice.ts
@@ -44,6 +44,18 @@ export const fetchAllPatients = createAsyncThunk<PatientData, number>(
   }
 );
 
+export const fetchPatient = createAsyncThunk<PatientData, number>(
+  'patients/fetchPatient',
+  async (id: number, thunkAPI) => {
+    try {
+      const res = await CLIENT.get(`${BASE_URL}/api/patients/patient/${id}`);
+      return res.data;
+    } catch (error) {
+      console.log('Redux error: ', error);
+    }
+  }
+);
+
 export const patientSlice = createSlice({
   name: 'patients',
   initialState,
@@ -62,6 +74,21 @@ export const patientSlice = createSlice({
         }
       )
       .addCase(fetchAllPatients.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload as string;
+      })
+      .addCase(fetchPatient.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(
+        fetchPatient.fulfilled,
+        (state, action: PayloadAction<PatientData>) => {
+          state.loading = false;
+          state.data = action.payload;
+        }
+      )
+      .addCase(fetchPatient.rejected, (state, action) => {
         state.loading = false;
         state.error = action.payload as string;
       });
