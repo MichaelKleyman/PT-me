bce7339116341ad8b9b675fd8b972e59849f4177
Michael Kleyman <michaelkleyman99@gmail.com>
2023-07-29 10:50:08 -0400

    setup post function to add the exercise to the flowsheet

--- server/routes/schedule.js
+++ server/routes/schedule.js
@@ -1,5 +1,10 @@
 const router = require("express").Router();
-const { Schedule, ScheduleExercise, Exercises } = require("../models");
+const {
+  Schedule,
+  ScheduleExercise,
+  Exercises,
+  PatientExercises,
+} = require("../models");
 
 //GET individual schedule
 router.get("/patient/:patientId", async (req, res, next) => {
@@ -82,23 +87,24 @@ router.put(
 
 //add patients exercise to the patients flowsheet
 router.post(
-  "/patient/:patientId/new-exercise/:scheduleExerciseId",
+  "/patient/:patientId/new-exercise/:scheduleExerciseId/:exerciseId",
   async (req, res, next) => {
     try {
-      // console.log(req.body);
-      const exercise = await Schedule.findOne({
+      const { scheduleId, exerciseId, sets, reps } = req.body;
+      const exercise = await ScheduleExercise.create({
+        scheduleId,
+        exerciseId,
+        sets,
+        reps,
+      });
+      const patientExercise = await PatientExercises.findOne({
         where: {
           patientId: req.params.patientId,
+          exerciseId: req.params.exerciseId,
         },
-        include: [
-          {
-            model: ScheduleExercise,
-            as: "exercises",
-            where: { id: req.params.scheduleExerciseId },
-          },
-        ],
       });
-      console.log(exercise);
+      await patientExercise.destroy();
+      res.send(exercise);
     } catch (error) {
       console.log("ERROR: >>>>>", error);
       next(error);
