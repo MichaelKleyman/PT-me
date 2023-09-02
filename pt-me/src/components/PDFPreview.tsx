c507a674877c1ae4eb43b5f1134a5844aed1c881
Michael Kleyman <michaelkleyman99@gmail.com>
2023-09-02 15:18:44 -0400

    added some styling to the pdf preview page

--- pt-me/src/components/PDFPreview.tsx
+++ pt-me/src/components/PDFPreview.tsx
@@ -7,53 +7,125 @@ const courier_prime = Courier_Prime({
 });
 
 interface Props {
-    patientFormData: PatientFormData
+  patientFormData: PatientFormData;
 }
 
-export default function PDFPreview({patientFormData}: Props) {
+export default function PDFPreview({ patientFormData }: Props) {
+  console.log(patientFormData);
   return (
     <div
       className={`${courier_prime.className} p-9 bg-[#f3f8f7] shadow-lg shadow-slate-400 grid grid-cols-3 gap-4`}
     >
       <div>
         <label>First Name</label>
-        <div className='border-b-[1px] border-black p-2'></div>
+        <div className=' p-2'>
+          {patientFormData["First Name"].length > 0 ? (
+            patientFormData["First Name"]
+          ) : (
+            <>&nbsp;</> // non breaking space entity
+          )}
+        </div>
+        <div className='border-b-[1px] border-black'></div>
       </div>
       <div>
         <label>Last Name</label>
-        <div className='border-b-[1px] border-black p-2'></div>
+        <div className='p-2'>
+          {patientFormData["Last Name"].length > 0 ? (
+            patientFormData["Last Name"]
+          ) : (
+            <>&nbsp;</> // non breaking space entity
+          )}
+        </div>
+        <div className='border-b-[1px] border-black'></div>
       </div>
       <div>
         <label>Gender</label>
-        <div className='border-b-[1px] border-black p-2'></div>
+        <div className='p-2'>
+          {patientFormData.Gender.length > 0 ? (
+            patientFormData.Gender
+          ) : (
+            <>&nbsp;</> // non breaking space entity
+          )}
+        </div>
+        <div className='border-b-[1px] border-black'></div>
       </div>
       <div>
         <label>Email</label>
-        <div className='border-b-[1px] border-black p-2'></div>
+        <div className=' p-2'>
+          {patientFormData.Email.length > 0 ? (
+            patientFormData.Email
+          ) : (
+            <>&nbsp;</> // non breaking space entity
+          )}
+        </div>
+        <div className='border-b-[1px] border-black'></div>
       </div>
       <div>
         <label>Phone Number</label>
-        <div className='border-b-[1px] border-black p-2'></div>
+        <div className=' p-2'>
+          {patientFormData["Phone Number"] > 0 ? (
+            patientFormData["Phone Number"]
+          ) : (
+            <>&nbsp;</> // non breaking space entity
+          )}
+        </div>
+        <div className='border-b-[1px] border-black'></div>
       </div>
       <div>
         <label>Insurance</label>
-        <div className='border-b-[1px] border-black p-2'></div>
+        <div className=' p-2'>
+          {patientFormData.Insurance.length > 0 ? (
+            patientFormData.Insurance
+          ) : (
+            <>&nbsp;</> // non breaking space entity
+          )}
+        </div>
+        <div className='border-b-[1px] border-black'></div>
       </div>
       <div>
         <label>Address</label>
-        <div className='border-b-[1px] border-black p-2'></div>
+        <div className=' p-2'>
+          {patientFormData.Address.length > 0 ? (
+            patientFormData.Address
+          ) : (
+            <>&nbsp;</> // non breaking space entity
+          )}
+          {/* {patientFormData.Address.length && patientFormData.Address} */}
+        </div>
+        <div className='border-b-[1px] border-black'></div>
       </div>
       <div>
         <label>State</label>
-        <div className='border-b-[1px] border-black p-2'></div>
+        <div className=' p-2'>
+          {patientFormData.State.length > 0 ? (
+            patientFormData.State
+          ) : (
+            <>&nbsp;</> // non breaking space entity
+          )}
+        </div>
+        <div className='border-b-[1px] border-black'></div>
       </div>
       <div>
         <label>City</label>
-        <div className='border-b-[1px] border-black p-2'></div>
+        <div className=' p-2'>
+          {patientFormData.City.length > 0 ? (
+            patientFormData.City
+          ) : (
+            <>&nbsp;</> // non breaking space entity
+          )}
+        </div>
+        <div className='border-b-[1px] border-black'></div>
       </div>
       <div>
         <label>Zipcode</label>
-        <div className='border-b-[1px] border-black p-2'></div>
+        <div className=' p-2'>
+          {patientFormData.Zipcode ? (
+            patientFormData.Zipcode
+          ) : (
+            <>&nbsp;</> // non breaking space entity
+          )}
+        </div>
+        <div className='border-b-[1px] border-black'></div>
       </div>
     </div>
   );
