bcfebd3788ff840f578c84868eaf038b4fa16cbf
Michael Kleyman <michaelkleyman99@gmail.com>
2023-09-02 12:26:09 -0400

    building the foundation of the react-hook-form logic for the create patient form

--- pt-me/src/components/CreatePatientForm.tsx
+++ pt-me/src/components/CreatePatientForm.tsx
@@ -1,5 +1,6 @@
 "use client";
 import TextField from "@mui/material/TextField";
+import { useState } from "react";
 import { Controller, useController, useForm } from "react-hook-form";
 
 const styling = { width: "100%", borderRadius: "10px", margin: "10px" };
@@ -19,15 +20,18 @@ const inputs = [
 ];
 
 export default function CreatePatientForm() {
+  const [page, setPage] = useState<number>(0);
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm({ mode: "all" });
 
+  const onSubmit = () => {};
+
   return (
     <div className='grid md:grid-cols-3 gap-6 py-5'>
-      <TextField type='text' required label='Last Name' sx={styling} />
+      {/* <TextField type='text' required label='Last Name' sx={styling} />
       <TextField type='text' required label='First Name' sx={styling} />
       <TextField type='number' required label='Age' sx={styling} />
       <TextField type='number' required label='Gender' sx={styling} />
@@ -41,7 +45,14 @@ export default function CreatePatientForm() {
       <div>
         <label className='ml-3 font-medium text-green-500'>Arrival Date</label>
         <TextField type='date' required sx={styling} />
-      </div>
+      </div> */}
+      <form onSubmit={handleSubmit(onSubmit)} className='w-[55%]'>
+        {inputs.map((question, index) => (
+          <div key={index}>
+            <TextField type='text' required label={question} sx={styling} />
+          </div>
+        ))}
+      </form>
     </div>
   );
 }
