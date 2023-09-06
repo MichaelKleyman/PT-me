d3c4919550fdd3bc961cdd583d872c84bf97b171
Michael Kleyman <michaelkleyman99@gmail.com>
2023-09-06 15:40:06 -0400

    added a loading state within the dashboard in order to not have flickers of changing UI occur on reload

--- pt-me/src/app/page.tsx
+++ pt-me/src/app/page.tsx
@@ -1,25 +1,35 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-'use client';
-import React, { useEffect } from 'react';
-import AboutUs from '@/components/AboutUs';
-import Services from '@/components/Services';
-import { Inter } from 'next/font/google';
-import { useSelector, useDispatch } from 'react-redux';
-import { RootState, AppDispatch } from '../Redux/store';
-import { me } from '../Redux/Features/auth/authSlice';
-import Dashboard from '@/components/Dashboard';
+"use client";
+import React, { useState, useEffect } from "react";
+import AboutUs from "@/components/AboutUs";
+import Services from "@/components/Services";
+import { Inter } from "next/font/google";
+import { useSelector, useDispatch } from "react-redux";
+import { RootState, AppDispatch } from "../Redux/store";
+import { me } from "../Redux/Features/auth/authSlice";
+import Dashboard from "@/components/Dashboard";
+import CircularProgress from "@mui/material/CircularProgress";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [isLoading, setLoading] = useState<boolean>(true);
   const dispatch = useDispatch<AppDispatch>();
   const user = useSelector((state: RootState) => state.auth.user);
   // console.log(user);
 
   useEffect(() => {
     dispatch(me());
+    setLoading(false);
   }, []);
 
+  if (isLoading) {
+    return (
+      <div className='flex items-center justify-center mt-[20rem]'>
+        <CircularProgress />
+      </div>
+    );
+  }
   return (
     <main className='mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl mt-[5rem]'>
       {!user?.id ? (
