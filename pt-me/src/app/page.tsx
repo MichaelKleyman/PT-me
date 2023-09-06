42063e54195bdb85ff53cc737b833190ccdc865b
Michael Kleyman <michaelkleyman99@gmail.com>
2023-09-06 15:45:02 -0400

    moved the original loading state that avoided unintended UI flickers to the layout file in order to apply it to the entire application therefore ridding any UI flickers

--- pt-me/src/app/page.tsx
+++ pt-me/src/app/page.tsx
@@ -8,28 +8,18 @@ import { useSelector, useDispatch } from "react-redux";
 import { RootState, AppDispatch } from "../Redux/store";
 import { me } from "../Redux/Features/auth/authSlice";
 import Dashboard from "@/components/Dashboard";
-import CircularProgress from "@mui/material/CircularProgress";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const [isLoading, setLoading] = useState<boolean>(true);
   const dispatch = useDispatch<AppDispatch>();
   const user = useSelector((state: RootState) => state.auth.user);
   // console.log(user);
 
   useEffect(() => {
     dispatch(me());
-    setLoading(false);
   }, []);
 
-  if (isLoading) {
-    return (
-      <div className='flex items-center justify-center mt-[20rem]'>
-        <CircularProgress />
-      </div>
-    );
-  }
   return (
     <main className='mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl mt-[5rem]'>
       {!user?.id ? (
--- pt-me/src/components/layoutfile.tsx
+++ pt-me/src/components/layoutfile.tsx
@@ -1,24 +1,34 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-'use client'; //this will be a client component
-import { useEffect } from 'react';
-import Navbar from '../components/Navbar';
-import Navbar2 from '../components/Navbar2';
-import { useSelector, useDispatch } from 'react-redux';
-import { RootState, AppDispatch } from '../Redux/store';
-import { me } from '../Redux/Features/auth/authSlice';
+"use client"; //this will be a client component
+import { useState, useEffect } from "react";
+import Navbar from "../components/Navbar";
+import Navbar2 from "../components/Navbar2";
+import { useSelector, useDispatch } from "react-redux";
+import { RootState, AppDispatch } from "../Redux/store";
+import { me } from "../Redux/Features/auth/authSlice";
+import CircularProgress from "@mui/material/CircularProgress";
 
 export default function Layoutfile({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const [isLoading, setLoading] = useState<boolean>(true);
   const dispatch = useDispatch<AppDispatch>();
   const user = useSelector((state: RootState) => state.auth.user);
 
   useEffect(() => {
     dispatch(me());
+    setLoading(false);
   }, []);
 
+  if (isLoading) {
+    return (
+      <div className='flex items-center justify-center mt-[20rem]'>
+        <CircularProgress />
+      </div>
+    );
+  }
   return (
     <>
       <Navbar2 />
