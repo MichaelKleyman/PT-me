3c94ae9a88e359cf48b9da6e8723013d57f664e8
Michael Kleyman <michaelkleyman99@gmail.com>
2023-09-13 14:38:07 -0400

    added all necessary text fields to the create exercise form and now I will add the proper useHook react form functionality

--- pt-me/src/components/CreateExerciseForm.tsx
+++ pt-me/src/components/CreateExerciseForm.tsx
@@ -50,18 +50,47 @@ export default function CreateExerciseForm() {
           label='Exercise Description'
         />
       </div>
-      <div className='flex items-center w-[50%]'>
-        <TextField
-          type='text'
-          required
-          label='Add Tips For The Exercise'
-          sx={{ width: "100%", borderRadius: "10px", margin: "10px" }}
-          {...register("exerciseName", {
-            required: true,
-          })}
-        />
-        <button className='h-[40px] w-[30%] bg-[#3BE13B] rounded-lg shadow-green-400 shadow-lg duration-300 hover:scale-110 text-white'>
-          Add
+      <div className='flex'>
+        <div className='w-full'>
+          <div className='flex items-center w-[100%]'>
+            <TextField
+              type='text'
+              required
+              label='Add Tips For The Exercise'
+              sx={{ width: "100%", borderRadius: "10px", margin: "10px" }}
+              {...register("exerciseName", {
+                required: true,
+              })}
+            />
+            <button className='h-[40px]  bg-[#3BE13B] w-[30%] rounded-lg shadow-green-400 shadow-lg duration-300 hover:scale-110 text-white'>
+              Add
+            </button>
+          </div>
+          <div className='p-3 bg-slate-100 rounded-sm shadow-lg shadow-gray-200 w-[80%] h-[140px]'>
+            Tips
+          </div>
+        </div>
+        <div className='w-full'>
+          <TextField
+            type='text'
+            required
+            label='Link To Exercise Instruction Video'
+            sx={{ width: "100%", borderRadius: "10px", margin: "10px" }}
+            {...register("exerciseName", {
+              required: true,
+            })}
+          />
+        </div>
+      </div>
+      <div className='flex items-center justify-center mt-[2rem] gap-4'>
+        <button className='w-[20%] hover:bg-[#3BE13B] hover:text-white bg-white text-[#3BE13B] rounded-lg shadow-green-400 shadow-lg duration-300 hover:scale-110  px-4 py-2'>
+          Cancel
+        </button>
+        <button
+          type='submit'
+          className='w-[20%] bg-[#3BE13B] rounded-lg shadow-green-400 shadow-lg duration-300 hover:scale-110 text-white px-4 py-2'
+        >
+          Submit
         </button>
       </div>
     </div>
