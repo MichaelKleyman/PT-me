d521e51262c217d9f9122c8e7dfc945933d959d4
Michael Kleyman <michaelkleyman99@gmail.com>
2023-09-26 17:09:53 -0400

    Added a get route for all changes made to a specific exercise

--- server/routes/exEditCredentials.js
+++ server/routes/exEditCredentials.js
@@ -1,6 +1,25 @@
 const router = require("express").Router();
 const { Exercise_Edited_Credentials } = require("../models");
 
+//GET exercise edit history based on exerciseID
+router.get("/credential-history/:exerciseId", async (req, res, next) => {
+  try {
+    const credential = await Exercise_Edited_Credentials.findAll({
+      where: {
+        ex_id: req.params.exerciseId,
+      },
+      order: [["createdAt", "DESC"]],
+    });
+    if (!credential) {
+      return res.status(404).send({ message: "Credential cant be fetched" });
+    }
+    return res.send(credential);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
 //POST a new person editing the exercise data
 router.post(`/new-edit/:exerciseId`, async (req, res, next) => {
   try {
