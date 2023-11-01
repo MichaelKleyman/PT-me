cd03f3b7805e99c9250a9e4c21a6d6088029e163
Michael Kleyman <michaelkleyman99@gmail.com>
2023-10-31 20:38:02 -0400

    fixed the footer

--- pt-me/src/components/Footer.tsx
+++ pt-me/src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { AiOutlineCopyrightCircle } from "react-icons/ai";
 
 export default function Footer() {
   return (
-    <div className='mt-[4rem] py-[2rem] flex items-center justify-center gap-2 text-gray-400 text-[13px]'>
+    <div className='mt-[8rem] py-[1rem] flex items-center justify-center gap-2 text-gray-400 text-[13px]'>
       <AiOutlineCopyrightCircle /> PT-me. 2023, All rights reserved.
     </div>
   );
